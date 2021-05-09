b0f32796ba09f4cf9aec819103b57f454d4d2f75
paddyyates7890 <64584807+paddyyates7890@users.noreply.github.com>
2021-05-09 20:52:03 +0100

    Add files via upload

---
+++ fibonacisequence/fibonacisequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fibonacisequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fibonachi();
+
+
+        }
+       
+        public static int fibonachi(int final = 1) 
+        {
+            bool run = true;
+            int num1 = 0;
+            int num2 = 1;
+
+            Console.WriteLine(0);
+            System.Threading.Thread.Sleep(2000);
+            while (run)
+            {
+                Console.WriteLine(final);
+                System.Threading.Thread.Sleep(2000);
+                final = num1 + num2;
+                num1 = num2;
+                num2 = final;
+            } 
+            return 0;
+        }
+    }
+}
